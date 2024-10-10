36261041388c2a7cac1da7e232aba080a0693615
MechaLarry <mecha.larry@gmail.com>
2024-10-11 01:09:14 +0300

    recursion error ruins the tests

--- server/models.py
+++ server/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from sqlalchemy import MetaData, ForeignKey
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
@@ -11,22 +11,62 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 
-class Customer(db.Model):
+class Customer(db.Model, SerializerMixin):
     __tablename__ = 'customers'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
+    # Relationship with Review
+    reviews = db.relationship('Review', back_populates='customer')
+
+    # Establishes the many-to-many relationship with Item through reviews
+    items = db.relationship('Item', secondary='reviews',
+                            back_populates='customers', overlaps="reviews")
+    
+    # Exclude relationships from serialization to prevent recursion
+    serialize_rules = ('-reviews.customer', '-items.customers')
+
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
 
 
-class Item(db.Model):
+class Item(db.Model, SerializerMixin):
     __tablename__ = 'items'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     price = db.Column(db.Float)
 
+    # Relationship with Review
+    reviews = db.relationship('Review', back_populates='item')
+
+    # Establishes the many-to-many relationship with Customer through reviews
+    customers = db.relationship('Customer', secondary='reviews',
+                                back_populates='items', overlaps="reviews")
+    
+    serialize_rules = ('-reviews.item', '-customers.items')
+
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
+
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+
+    # Foreign keys
+    customer_id = db.Column(db.Integer, ForeignKey('customers.id'))
+    item_id = db.Column(db.Integer, ForeignKey('items.id'))
+
+    # Relationships
+    customer = db.relationship('Customer', back_populates='reviews', overlaps="customers,items")
+    item = db.relationship('Item', back_populates='reviews', overlaps="customers,items")
+
+    # Exclude relationships from serialization to prevent recursion
+    serialize_rules = ('-customer.reviews', '-item.reviews')
+
+    def __repr__(self):
+        return f'<Review {self.id}, {self.comment}>'
